1eca37296c043218ab04e2e8891227e9adb64345
willy <willington.londono@ucaldas.edu.co>
2023-04-11 20:24:15 -0500

    primer commit

---
+++ src/test/java/edu/ucaldas/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
